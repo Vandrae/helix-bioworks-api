dcbdc2ee59acaa1d1127a938da82a8c683644772
Gabe <Carrollgbusiness@gmail.com>
2026-07-20 19:30:10 -0500

    Import Genome from helix-bioworks

--- src/main/java/com/pluralsight/helix/organism/Genome.java
+++ src/main/java/com/pluralsight/helix/organism/Genome.java
@@ -1,4 +1,11 @@
 package com.pluralsight.helix.organism;
 
+//fixed list of options where only one option can be selected
 public enum Genome {
+    PRIMATE,
+    INSECTOID,
+    REPTILIAN,
+    AVIAN,
+    AQUATIC,
+    APEX
 }
