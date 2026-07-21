8935057e74e68186c17788173615bc3de688a524
Gabe <Carrollgbusiness@gmail.com>
2026-07-20 19:39:41 -0500

    Import TransportPackage from helix-bioworks

--- src/main/java/com/pluralsight/helix/order/TransportPackage.java
+++ src/main/java/com/pluralsight/helix/order/TransportPackage.java
@@ -1,4 +1,20 @@
 package com.pluralsight.helix.order;
 
 public class TransportPackage {
+    private final Size size;
+
+    //methods
+    public Credits getPrice(){
+        return size.getPrice();
+    }
+
+    //constructor
+    public TransportPackage(Size size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return " " + size + " " + getPrice();
+    }
 }
