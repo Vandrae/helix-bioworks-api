5194645f13db621e8c2b4ddec2ff27d646007bd2
Gabe <Carrollgbusiness@gmail.com>
2026-07-20 19:35:44 -0500

    Import Maintenance from helix-bioworks

--- src/main/java/com/pluralsight/helix/order/Maintenance.java
+++ src/main/java/com/pluralsight/helix/order/Maintenance.java
@@ -1,4 +1,15 @@
 package com.pluralsight.helix.order;
 
 public class Maintenance {
+
+    //set price of a maintenance package
+    public Credits getPrice(){
+        return new Credits(150.00);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Standard Maintenance " + " " + getPrice();
+    }
 }
