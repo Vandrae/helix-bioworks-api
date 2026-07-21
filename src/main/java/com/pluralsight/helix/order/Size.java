884a60b26c4048b68ad7d590867c2e0f5162e164
Gabe <Carrollgbusiness@gmail.com>
2026-07-20 19:39:15 -0500

    Import Size from helix-bioworks

--- src/main/java/com/pluralsight/helix/order/Size.java
+++ src/main/java/com/pluralsight/helix/order/Size.java
@@ -1,4 +1,28 @@
 package com.pluralsight.helix.order;
 
 public enum Size {
+    SMALL,
+    MEDIUM,
+    LARGE,
+    EXTRA_LARGE;
+
+    public Credits getPrice(){
+        switch (this){
+            case SMALL -> {
+                return new Credits(500);
+            }
+            case MEDIUM -> {
+                return new Credits(1000);
+            }
+            case LARGE -> {
+                return new Credits(2000);
+            }
+            case EXTRA_LARGE -> {
+                return new Credits(5000);
+            }default -> throw new IllegalStateException("Unknown Size");
+        }
+    }
+
+
+
 }
