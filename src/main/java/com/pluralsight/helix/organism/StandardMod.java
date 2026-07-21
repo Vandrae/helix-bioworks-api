1ad17b6209a540bae108d0c2807c65b187777856
Gabe <Carrollgbusiness@gmail.com>
2026-07-20 19:34:28 -0500

    Import StandardMod from helix-bioworks

--- src/main/java/com/pluralsight/helix/organism/StandardMod.java
+++ src/main/java/com/pluralsight/helix/organism/StandardMod.java
@@ -1,4 +1,26 @@
 package com.pluralsight.helix.organism;
 
 public class StandardMod {
+
+    private String name;
+
+    //methods
+    @Override
+    public String toString() {
+        return "Standard Modifications: " + name;
+    }
+
+    //constructor
+    public StandardMod(String name) {
+        this.name = name;
+    }
+
+    //getter/setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
